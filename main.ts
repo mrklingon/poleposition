405d7317064b2956c56e6f254d4eb8856f84f926
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-12-18 20:18:57 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 function mkRoad () {
     for (let index = 0; index < 100; index++) {
-        road.push(randint(0, 3))
+        road.push(randint(0, 2))
     }
+    road[0] = 2
+    road[1] = 2
+    road[2] = 2
+    road[3] = 2
+    road[4] = 2
 }
 input.onButtonPressed(Button.A, function () {
     carx += -1
@@ -21,15 +26,23 @@ function shoRoad () {
         line = road[index]
         for (let index2 = 0; index2 <= 4; index2++) {
             for (let index3 = 0; index3 <= line; index3++) {
-                led.plotBrightness(index3, index, 128)
+                led.plotBrightness(index3, 4 - index, 128)
+            }
+            if (line < 2) {
+                led.plotBrightness(4, 4 - index, 128)
+            }
+            if (line < 1) {
+                led.plotBrightness(3, 4 - index, 128)
             }
         }
     }
     road.push(road.removeAt(0))
     if (led.pointBrightness(carx, cary) > 0) {
+        game.setScore(tick)
         game.removeLife(1)
     } else {
         led.plot(carx, cary)
+        tick += 1
     }
 }
 input.onButtonPressed(Button.B, function () {
@@ -38,13 +51,21 @@ input.onButtonPressed(Button.B, function () {
         carx = 4
     }
 })
+let tick = 0
 let line = 0
 let cary = 0
 let carx = 0
 let road: number[] = []
+images.createBigImage(`
+    . . . . . . . . . .
+    . . . . # # # . . .
+    . . # # . . # # . .
+    . # # # # # # # # .
+    . . # # . . # # . .
+    `).scrollImage(1, 200)
 road = []
 mkRoad()
-carx = 4
+carx = 3
 cary = 4
 game.setLife(5)
 basic.forever(function () {
